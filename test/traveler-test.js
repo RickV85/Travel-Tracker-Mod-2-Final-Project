07f0cd6fc151678e0be199b6738d8311c629ebe9
Rick Vermeil <rickv85@gmail.com>
2023-01-11 15:17:45 -0700

    Add calculateSpendInLastYear method to Traveler

--- test/traveler-test.js
+++ test/traveler-test.js
@@ -7,12 +7,15 @@ import destinations from '../test/destinations-test-data'
 
 
 describe('Traveler', function() {
-  let testTraveler = travelers[0];
-  let testTrips = trips;
-  let testDestinations = destinations;
+  let testTraveler;
+  let testTrips;
+  let testDestinations;
 
   this.beforeEach(() => {
-    testTraveler = new Traveler(testTraveler);
+    testTrips = trips;
+    testDestinations = destinations;
+    testTraveler = new Traveler(travelers[0]);
+    testTraveler.addPastTrips(testTrips);
   });
 
   it('should instantiate new travelers', function() {
@@ -32,11 +35,10 @@ describe('Traveler', function() {
   });
 
   it('should have a pastTrips property', function() {
-    expect(testTraveler.pastTrips).to.deep.equal([]);
+    expect(testTraveler).to.haveOwnProperty('pastTrips');
   });
 
-  it('should have a function to add past trips to pastTrips property', function() {
-    testTraveler.addPastTrips(testTrips);
+  it('should have a method to add past trips to pastTrips property', function() {
     expect(testTraveler.pastTrips).to.deep.equal(
       [
       {
@@ -54,8 +56,59 @@ describe('Traveler', function() {
   });
 
   it('should have properties for upcoming and pending trips', function() {
-    expect(testTraveler.upcomingTrips).to.deep.equal([]);
-    expect(testTraveler.pendingTrips).to.deep.equal([]);
+    expect(testTraveler).to.haveOwnProperty('upcomingTrips');
+    expect(testTraveler).to.haveOwnProperty('pendingTrips');
   })
 
+  it('should have a property for total amount spent on trips in the last 365 days', function() {
+    expect(testTraveler).to.haveOwnProperty('amountSpentInLastYear');
+  })
+
+  it('should have a function to calculate amount spent in the last 365 days plus 10% fee', function() {
+    testTraveler.calculateSpendInLastYear();
+    expect(testTraveler.amountSpentInLastYear).to.equal(0);
+
+    testTraveler.pastTrips = [
+      {
+      id: 117,
+      userID: 1,
+      destinationID: 28,
+      travelers: 3,
+      date: "2021/01/09",
+      duration: 15,
+      status: "approved",
+      suggestedActivities: [ ]
+      },
+      {
+      id: 118,
+      userID: 1,
+      destinationID: 1,
+      travelers: 1,
+      date: "2022/06/09",
+      duration: 3,
+      status: "approved",
+      suggestedActivities: [ ]
+      },
+      {
+      id: 119,
+      userID: 1,
+      destinationID: 2,
+      travelers: 2,
+      date: "2022/08/09",
+      duration: 3,
+      status: "approved",
+      suggestedActivities: [ ]
+      }
+    ]
+
+    testTraveler.calculateSpendInLastYear();
+    expect(testTraveler.amountSpentInLastYear).to.equal(2717);
+  })
+
+  it('should be able to calculate total spend in last year for any user', function() {
+    testTraveler = new Traveler(travelers[2]);
+    testTraveler.addPastTrips(testTrips);
+    testTraveler.calculateSpendInLastYear();
+    expect(testTraveler.amountSpentInLastYear).to.equal(4543);
+  })
 });
