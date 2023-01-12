95dce86e06794ffd5fd16f60d9195511d2e27571
Rick Vermeil <rickv85@gmail.com>
2023-01-12 15:48:10 -0700

    Instantiate a user

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,7 @@
 // Imports
 import './css/styles.css';
 import apicalls from './apiCalls';
+import Traveler from './traveler';
 
 // Promises
 let allTravelersPromise = apicalls.getAllTravelers();
@@ -10,6 +11,8 @@ let allDestinationsPromise = apicalls.getAllDestinations();
 
 // Global variables
 let currentTraveler;
+let allTrips;
+let allDestinations;
 
 // Query selectors
 
@@ -17,7 +20,8 @@ let currentTraveler;
 // Event listeners
 window.addEventListener('load', () => {
   // Get single user here with singleTravelerPromise = apicalls.getSingleTraveler(id);
-  singleTravelerPromise = apicalls.getSingleTraveler(4);
+  singleTravelerPromise = apicalls.getSingleTraveler(3);
+
   resolvePromises();
 })
 
@@ -26,5 +30,17 @@ function resolvePromises() {
   Promise.all([allTravelersPromise, singleTravelerPromise, allTripsPromise, allDestinationsPromise])
     .then(data => {
       console.log(data)
+      currentTraveler = data[1];
+      allTrips = data[2].trips;
+      allDestinations = data[3];
+      instatiateCurrentTraveler();
     })
+}
+
+// Add id parameter after log in is created to make this dynamic
+function instatiateCurrentTraveler() {
+  currentTraveler = new Traveler(currentTraveler);
+  currentTraveler.addPastTrips(allTrips);
+  currentTraveler.calculateSpendInLastYear();
+  console.log('currentTraveler', currentTraveler)
 }
